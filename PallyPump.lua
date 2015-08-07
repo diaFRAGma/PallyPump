8d836f35b0a3d0043659a7df24da003b9e8aa6b4
diaFRAGma <diaFRAGma@users.noreply.github.com>
2015-08-07 15:02:38 +0200

    Es gibt nun eine temporäre out of sight ignore list

--- PallyPump.lua
+++ PallyPump.lua
@@ -2,40 +2,51 @@ local actionSlotWithFlashHeal = 1
 local lichtblitzSchwelle = 1
 local heiligesLichtSchwelle = 1500
 local unitToHeal = "nobody"
-local losMerker = nil
+local ignoreTime = 10 -- Sekunden
+local ignoreList = {}
+
+--TODO prüfen ob auf Slot 1 auch Lichtblitz ist ansonsten fehler ausgeben
 
 function PallyPump()
-	-- Göttliche Gunst  0 = kein CD
-	-- Göttliche Gunst -1 = Buff ist Aktiv (CD wird erst gestartet wenn verbraucht)
-	-- Göttliche Gunst >0 = hat noch CD
-	local gg = getCooldown("Göttliche Gunst")
+	if GetActionTexture(actionSlotWithFlashHeal) == "Interface\\Icons\\Spell_Holy_FlashHeal" and ActionHasRange(actionSlotWithFlashHeal) == 1 then
+		cleanLosList()
+		-- Göttliche Gunst  0 = kein CD
+		-- Göttliche Gunst -1 = Buff ist Aktiv (CD wird erst gestartet wenn verbraucht)
+		-- Göttliche Gunst >0 = hat noch CD
+		local gg = getCooldown("Göttliche Gunst")
 	
-	getMemberToHeal()
-	if unitToHeal ~= "nobody" then
-		DEFAULT_CHAT_FRAME:AddMessage(UnitName(unitToHeal).." wird geheilt.")
-	else
-		DEFAULT_CHAT_FRAME:AddMessage("Niemand braucht Heilung.")
-	end
+		setUnitToHeal()
+		--if unitToHeal ~= "nobody" then
+			--DEFAULT_CHAT_FRAME:AddMessage(UnitName(unitToHeal).." wird geheilt.")
+		--else
+			--DEFAULT_CHAT_FRAME:AddMessage("Niemand braucht Heilung.")
+		--end
 	
-	if unitToHeal ~= "nobody" then
-		local max_health = UnitHealthMax(unitToHeal);
-		local health = UnitHealth(unitToHeal);
-		local diff = max_health-health;
-		if diff >= heiligesLichtSchwelle then
-			if gg == 0 then
-				CastSpellByName("G\195\182ttliche Gunst")
-			elseif gg == -1 or gg > 0 then
+		if unitToHeal ~= "nobody" then
+			local max_health = UnitHealthMax(unitToHeal)
+			local health = UnitHealth(unitToHeal)
+			local diff = max_health-health
+			if diff >= heiligesLichtSchwelle then
+				if gg == 0 then
+					CastSpellByName("G\195\182ttliche Gunst")
+				elseif gg == -1 then
+					TargetUnit(unitToHeal)
+					CastSpellByName("Heiliges Licht")
+					TargetLastTarget()
+				else
+					TargetUnit(unitToHeal)
+					CastSpellByName("Lichtblitz")
+					TargetLastTarget()				
+				end
+			elseif diff >= lichtblitzSchwelle then
 				TargetUnit(unitToHeal)
-				CastSpellByName("Heiliges Licht")
+				CastSpellByName("Lichtblitz")
 				TargetLastTarget()
 			end
-		elseif diff >= lichtblitzSchwelle then
-			TargetUnit(unitToHeal)
-			CastSpellByName("Lichtblitz")
-			TargetLastTarget()
 		end
+	else
+		DEFAULT_CHAT_FRAME:AddMessage("PallyPump konnte auf Slot "..actionSlotWithFlashHeal.." kein Lichtblitz finden." , 1.0, 0.0, 0.0)
 	end
-
 	--DEFAULT_CHAT_FRAME:AddMessage("Debug")
 end
 
@@ -47,7 +58,7 @@ function getCooldown(pSpell)
 			do break end
 		end
 		if spellName == pSpell then
-			local start, duration, enabled = GetSpellCooldown(i, BOOKTYPE_SPELL);
+			local start, duration, enabled = GetSpellCooldown(i, BOOKTYPE_SPELL)
 			if enabled == 0 then
 				-- Der Zauber ist gerade aktiv. Der CD startet erst wenn er verbraucht wurde.
 				return -1
@@ -63,18 +74,19 @@ function getCooldown(pSpell)
 	end
 end
 
-function getMemberToHeal()
-	local healthToHeal = 0;
+function setUnitToHeal()
+	unitToHeal = "nobody"
+	local healthToHeal = 0
 	if UnitInRaid("player") then
 		-- Der Spieler ist in einem Raid.
 		for raidIndex = 1, MAX_RAID_MEMBERS do
 			if GetRaidRosterInfo(raidIndex) then
-				local max_health = UnitHealthMax("raid"..raidIndex);
-				local health = UnitHealth("raid"..raidIndex);
-				--local name = UnitName("raid"..raidIndex);
+				local max_health = UnitHealthMax("raid"..raidIndex)
+				local health = UnitHealth("raid"..raidIndex)
+				--local name = UnitName("raid"..raidIndex)
 				TargetUnit("raid"..raidIndex)
 				inRange = IsActionInRange(actionSlotWithFlashHeal)
-				if max_health-health > healthToHeal and not UnitIsDead("raid"..raidIndex) and not UnitIsGhost("raid"..raidIndex) and UnitIsConnected("raid"..raidIndex) and UnitIsVisible("raid"..raidIndex) and inRange == 1 then
+				if max_health-health > healthToHeal and not UnitIsDead("raid"..raidIndex) and not UnitIsGhost("raid"..raidIndex) and UnitIsConnected("raid"..raidIndex) and UnitIsVisible("raid"..raidIndex) and inRange == 1 and ignoreList[UnitName("raid"..raidIndex)] == nil then
 					healthToHeal = max_health-health
 					unitToHeal = "raid"..raidIndex
 				end
@@ -86,12 +98,12 @@ function getMemberToHeal()
 		-- Der Spieler ist in einer Gruppe.
 		for groupIndex = 1, MAX_PARTY_MEMBERS do
 			if GetPartyMember(groupIndex) then
-				local max_health = UnitHealthMax("party"..groupIndex);
-				local health = UnitHealth("party"..groupIndex);
-				--local name = UnitName("party"..groupIndex);
+				local max_health = UnitHealthMax("party"..groupIndex)
+				local health = UnitHealth("party"..groupIndex)
+				--local name = UnitName("party"..groupIndex)
 				TargetUnit("party"..groupIndex)
-				inRange = IsActionInRange(actionSlotWithFlashHeal);
-				if max_health-health > healthToHeal and not UnitIsDead("party"..groupIndex) and not UnitIsGhost("party"..groupIndex) and UnitIsConnected("party"..groupIndex) and UnitIsVisible("party"..groupIndex) and inRange == 1 then
+				inRange = IsActionInRange(actionSlotWithFlashHeal)
+				if max_health-health > healthToHeal and not UnitIsDead("party"..groupIndex) and not UnitIsGhost("party"..groupIndex) and UnitIsConnected("party"..groupIndex) and UnitIsVisible("party"..groupIndex) and inRange == 1 and ignoreList[UnitName("party"..groupIndex)] == nil then
 					healthToHeal = max_health-health
 					unitToHeal = "party"..groupIndex
 				end
@@ -100,9 +112,9 @@ function getMemberToHeal()
 			end
 		end
 		-- Man muss sich selbst noch überprüfen, weil man mit getPartyMember nur alle anderen Mitglieder der Gruppe bekommt
-		local max_health = UnitHealthMax("player");
-		local health = UnitHealth("player");
-		--local name = UnitName("player");
+		local max_health = UnitHealthMax("player")
+		local health = UnitHealth("player")
+		--local name = UnitName("player")
 		if max_health-health > healthToHeal then
 			healthToHeal = max_health-health
 			unitToHeal = "player"
@@ -119,8 +131,21 @@ end
 function PallyPump_OnEvent()
 	if event == "UI_ERROR_MESSAGE" and arg1 == "Ziel ist nicht im Sichtfeld." then
 		--DEFAULT_CHAT_FRAME:AddMessage(UnitName(unitToHeal).." - "..arg1)
-		SendChatMessage("Heilung fehlgeschlagen. Grund: Nicht im Sichtfeld", "WHISPER", nil, UnitName(unitToHeal))
-		--für nächste runde ignorieren
+		SendChatMessage("Du bist nicht im Sichtfeld und somit 10 Sekunden auf Heal-Ignore", "WHISPER", nil, UnitName(unitToHeal))
+		ignoreList[UnitName(unitToHeal)] = time()
+		DEFAULT_CHAT_FRAME:AddMessage(UnitName(unitToHeal).." wird für "..ignoreTime.." Sekunden ignoriert.")
+	end
+end
+
+function cleanLosList()
+	local i = 0
+	for key,value in pairs(ignoreList) do
+		-- Wenn der Spieler seit x Sekunden oder mehr auf der Ignoreliste war wird er von dieser entfernt
+		if time() - value >= ignoreTime then
+			ignoreList[key] = nil
+			DEFAULT_CHAT_FRAME:AddMessage(key.." wird nun nicht mehr ignoriert.")
+		end
+		i = i + 1
 	end
 end
 
